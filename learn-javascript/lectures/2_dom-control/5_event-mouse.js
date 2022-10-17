031cbe9b7c38bf141da2a8921f2aadee2bdefb4a
oet <54170438+ohet9409@users.noreply.github.com>
2022-10-17 22:33:56 +0900

    이벤트 다루기2

--- learn-javascript/lectures/2_dom-control/5_event-mouse.js
+++ learn-javascript/lectures/2_dom-control/5_event-mouse.js
@@ -1,23 +1,41 @@
-console.log('5_event-mouse')
+console.log("5_event-mouse");
 
-let rectangle = document.querySelector('#rectangle')
+let rectangle = document.querySelector("#rectangle");
 
-rectangle.addEventListener('mousedown', function (e) {
-  console.log(e)
-})
+let body = document.querySelector("body");
+console.log(body);
 
-rectangle.addEventListener('mouseup', function (e) {
-  console.log(e)
-})
+rectangle.addEventListener("mousedown", function (e) {
+  console.log(e);
+  console.log("clientX: ", e.clientX, "clientY: ", e.clientY);
+  console.log("pageX: ", e.pageX, "pageY: ", e.pageY);
+});
 
-rectangle.addEventListener('mouseenter', function (e) {
-  console.log(e)
-})
+body.addEventListener("click", function (e) {
+  console.log("pageX: ", e.pageX, "pageY: ", e.pageY);
 
-rectangle.addEventListener('mouseleave', function (e) {
-  console.log(e)
-})
+  let div = document.createElement("div");
+  div.classList.add("circle");
+  div.style.left = e.pageX - 25 + "px";
+  div.style.top = e.pageY - 25 + "px";
+  // body.appendChild(div);
+  // body.classList.add("circle");
 
-rectangle.addEventListener('mousemove', function (e) {
-  console.log(e)
-})
+  console.log(e.target.getBoundingClientRect());
+});
+
+// rectangle.addEventListener('mouseup', function (e) {
+//   console.log(e)
+// })
+
+// rectangle.addEventListener('mouseenter', function (e) {
+//   console.log(e)
+// })
+
+// rectangle.addEventListener('mouseleave', function (e) {
+//   console.log(e)
+// })
+
+// rectangle.addEventListener('mousemove', function (e) {
+//   console.log(e)
+// })
